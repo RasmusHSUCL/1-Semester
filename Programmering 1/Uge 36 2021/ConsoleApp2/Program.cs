090766a50b7bedde8360fd3c59b668e6bd8cd7e4
RasmusHS <94040525+RasmusHSUCL@users.noreply.github.com>
2021-11-10 10:41:26 +0100

    Add files via upload

---
+++ Programmering 1/Uge 36 2021/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 255;
+            sbyte z = 126;
+            ushort y = 65535;
+            short Y = 32767; // 0111 1111 1111 1111
+            
+            int saldoAarTilDato = 2147483647; // 01111 1111 1111 1111 1111 1111 1111 1111
+            int saldo = -2147483647; // 32 bit
+            
+            long langTal = 0; // 64 bit
+            ulong ulangTal2 = 90;
+            
+            double decTal = 3.14; // decimaltal
+
+            // 1000 0000 0000 0000 0000 0000 0000 0000
+            x++;
+        }
+    }
+}
---
+++ Programmering 1/Uge 36 2021/Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp3
+{ // start namespace
+
+    class Program
+    { // start class
+
+        static void Main(string[] args)
+        { // start Main
+
+            string tal1;
+            Console.Write("Indtast et tal: ");
+
+            tal1 = Console.ReadLine();
+            
+
+
+            Console.WriteLine(tal1);
+            Console.ReadLine();
+
+        } // slut Main
+
+    } // slut class
+
+} // slut namespace
